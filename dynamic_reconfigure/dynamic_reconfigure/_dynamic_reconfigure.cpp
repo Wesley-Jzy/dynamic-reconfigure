b16ae978d332ec4a17d9653eb5b4ae514c05cfb4
xinliu <xin.liu@tusimple.com>
2018-06-28 18:08:52 +0800

    add my dynamic_reconfigure lib

---
+++ dynamic_reconfigure/dynamic_reconfigure/_dynamic_reconfigure.cpp
@@ -0,0 +1,249 @@
+#include <Python.h>
+#include "rclcpp/rclcpp.hpp"
+#include "rcl/rcl.h"
+#include "dynamicReconfServerPy.hpp" 
+#include "rqt_support.hpp"
+#include "rclcpp/parameter.hpp"
+
+static rclcpp::parameter::ParameterVariant _make_param(PyObject* dict, PyObject* key)
+{
+    std::string key_string= PyUnicode_AsUTF8(key);
+    PyObject* value=PyDict_GetItem(dict, key);
+
+    if(PyUnicode_Check(value)){
+        return rclcpp::parameter::ParameterVariant(key_string, PyUnicode_AsUTF8(value));
+    }
+
+    if(PyBool_Check(value)) {
+        PyObject_Print(value, stdout, Py_PRINT_RAW);
+        if (value==Py_True) {
+            return rclcpp::parameter::ParameterVariant(key_string, true);
+        } else {
+            return rclcpp::parameter::ParameterVariant(key_string, false);
+        }
+    }
+
+    if( PyLong_Check(value)) {
+        return rclcpp::parameter::ParameterVariant(key_string, PyLong_AsLong(value));
+    }
+
+    if( PyFloat_Check(value)) {
+        return rclcpp::parameter::ParameterVariant(key_string, PyFloat_AsDouble(value));
+    }
+    return rclcpp::parameter::ParameterVariant("", 0);
+}
+
+static void _set_dict(PyObject* dict, rclcpp::parameter::ParameterVariant parameter)
+{
+    switch(parameter.get_type()){
+        case rclcpp::parameter::ParameterType::PARAMETER_BOOL:
+            if(parameter.as_bool()) {
+                PyDict_SetItemString(dict, parameter.get_name().data(), Py_True);
+            } else {
+                PyDict_SetItemString(dict, parameter.get_name().data(), Py_False);
+            }
+            return;
+        case rclcpp::parameter::ParameterType::PARAMETER_INTEGER:
+            PyDict_SetItemString(dict, parameter.get_name().data(), PyLong_FromLong(parameter.as_int()));
+            return;
+        case rclcpp::parameter::ParameterType::PARAMETER_DOUBLE:
+            PyDict_SetItemString(dict, parameter.get_name().data(), PyFloat_FromDouble(parameter.as_double()));
+            return;
+        case rclcpp::parameter::ParameterType::PARAMETER_STRING:
+            PyDict_SetItemString(dict, parameter.get_name().data(), PyUnicode_FromString(parameter.as_string().data()));
+            return;
+        default:
+            ;
+    }
+}
+
+static PyObject *get_description(PyObject * Py_UNUSED(self), PyObject * args)
+{
+
+    char* remote_name;
+    if (!PyArg_ParseTuple(args, "z", &remote_name)) {
+        return NULL;
+    }
+
+    auto node = rclcpp::Node::make_shared("get_parameters_try_client");
+    std:: shared_ptr<rqt_reconfigure::Client> test_client = rqt_reconfigure::client_map.get_client(node, remote_name);
+    //rqt_reconfigure::Client test_client = rqt_reconfigure::Client(node, remote_name);
+    std::vector<rclcpp::parameter::ParameterVariant> descritions = test_client->get_description();
+    PyObject* dict=PyDict_New();
+    for (auto & parameter : descritions) {
+        _set_dict(dict, parameter);
+    }
+    //rclcpp::shutdown();
+    return dict;
+}
+static PyObject *get_values(PyObject * Py_UNUSED(self), PyObject * args)
+{
+    char* remote_name;
+    if (!PyArg_ParseTuple(args, "z", &remote_name)) {
+        return NULL;
+    }
+    printf("remote_name = %s\n", remote_name);
+    printf("before node_init\n");
+    auto node = rclcpp::Node::make_shared("get_parameters_try_client");
+    printf("after node_init\n");
+    printf("before client_init\n");
+    std:: shared_ptr<rqt_reconfigure::Client> test_client = rqt_reconfigure::client_map.get_client(node, remote_name);
+    //rqt_reconfigure::Client test_client = rqt_reconfigure::Client(node, remote_name);
+    //rqt_reconfigure::Client_map::check_map.begin();
+    //int temp = rqt_reconfigure::client_map.get_client(node, remote_name);
+    printf("after client_init\n");
+    std::vector<rclcpp::parameter::ParameterVariant> values = test_client->get_values();
+    printf("after get_description\n");
+    PyObject* dict=PyDict_New();
+    for (auto & parameter : values) {
+        _set_dict(dict, parameter);
+    }
+    //rclcpp::shutdown();
+    return dict;
+}
+
+static  PyObject *update_parameters(PyObject * Py_UNUSED(self), PyObject *args)
+{
+    //rcl_shutdown();
+    //auto parameters_client=_init_client("set_parameters_client");
+    printf("update_parameters\n");
+    printf("0\n");
+    PyObject* dict;
+    char* remote_name;
+    printf("1\n");
+    PyArg_ParseTuple(args, "zO", &remote_name, &dict);
+    printf("2\n");
+    //printf("remote_name = %s\n", remote_name);
+    PyObject* l=PyDict_Keys(dict);
+    printf("3\n");
+    Py_ssize_t i=0;
+    std::vector<rclcpp::parameter::ParameterVariant> para;
+    printf("4\n");
+    for(i=0;i<PyList_GET_SIZE(l);++i){
+        printf("i=%d\n",i);
+        PyObject* key=PyList_GetItem(l,i);
+        if(PyUnicode_Check(key)){
+            para.push_back(_make_param(dict, key));
+        }
+    }
+    printf("5\n");
+    auto node = rclcpp::Node::make_shared("get_parameters_try_client");
+    std:: shared_ptr<rqt_reconfigure::Client> test_client = rqt_reconfigure::client_map.get_client(node, remote_name);
+    //rqt_reconfigure::Client test_client = rqt_reconfigure::Client(node, remote_name);
+    test_client->update_params(para);
+
+    std::vector<rclcpp::parameter::ParameterVariant> values = test_client->get_values();
+
+    PyObject* ans=PyDict_New();
+    for (auto & parameter : values) {
+        _set_dict(ans, parameter);
+    }
+
+    return ans;
+
+}
+
+
+
+static rclcpp::parameter::ParameterVariant _make_param_add_name(PyObject* dict, PyObject* key, std::string name, std::string short_form)
+{
+    std::string key_string= short_form + '.' + name;
+    PyObject* value=PyDict_GetItem(dict, key);
+
+    if(PyUnicode_Check(value)){
+        return rclcpp::parameter::ParameterVariant(key_string, PyUnicode_AsUTF8(value));
+    }
+
+    if(PyBool_Check(value)) {
+        //PyObject_Print(value, stdout, Py_PRINT_RAW);
+        if (value==Py_True) {
+            return rclcpp::parameter::ParameterVariant(key_string, true);
+        } else {
+            return rclcpp::parameter::ParameterVariant(key_string, false);
+        }
+    }
+
+    if( PyLong_Check(value)) {
+        return rclcpp::parameter::ParameterVariant(key_string, PyLong_AsLong(value));
+    }
+
+    if( PyFloat_Check(value)) {
+        return rclcpp::parameter::ParameterVariant(key_string, PyFloat_AsDouble(value));
+    }
+    return rclcpp::parameter::ParameterVariant("", 0);
+}
+
+static  PyObject *params_service_init(PyObject * Py_UNUSED(self), PyObject *args)
+{
+    char* service_name;
+    PyObject *python_handle;
+    PyObject *cfg_list;
+    PyArg_ParseTuple(args, "zOO", &service_name, &python_handle, &cfg_list);
+
+    std::vector<rclcpp::parameter::ParameterVariant> cfg_all;
+
+    for(int i = 0; i < PyList_GET_SIZE(cfg_list); ++i) {
+        PyObject* param = PyList_GetItem(cfg_list, i);
+        PyObject* name = PyDict_GetItem(param, Py_BuildValue("s","name"));
+        std::string name_c = PyUnicode_AsUTF8(name);
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","name"), name_c, "name"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","description"), name_c, "des"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","type"), name_c, "type"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","level"), name_c, "lev"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","min"), name_c, "min"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","max"), name_c, "max"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","default"), name_c, "default"));
+        cfg_all.push_back(_make_param_add_name(param, Py_BuildValue("s","default"), name_c, "value"));
+    }
+    rqt_reconfigure::Server_py(service_name, python_handle, cfg_all);
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef module_methods[] =
+{
+    {
+        "params_service_init", params_service_init, METH_VARARGS,
+        "params_service_init"
+    },
+    {
+        "get_description", get_description, METH_VARARGS,
+        "get_description good"
+    },
+    {
+        "get_values", get_values, METH_VARARGS,
+        "get_values good"
+    },
+    {
+        "update_parameters", update_parameters, METH_VARARGS,
+        "update_parameters good"
+    },
+    {NULL, NULL, 0, NULL}  /* sentinel */
+};
+PyDoc_STRVAR(dynamic_reconfigure__doc__,
+            "dynamic_reconfig module: params_service_init");
+static struct PyModuleDef dynamic_reconfigure_module =
+{
+    PyModuleDef_HEAD_INIT,
+    "dynamic_reconfigure",
+    dynamic_reconfigure__doc__,
+    -1, /* -1 means that the module keeps state in global variables */
+    module_methods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+PyMODINIT_FUNC PyInit__dynamic_reconfigure_(void)
+{
+
+    PyObject *m;
+    m = PyModule_Create(&dynamic_reconfigure_module);
+    if( m == NULL)
+    {
+        return NULL;
+    }
+
+
+    return m;
+}
+
