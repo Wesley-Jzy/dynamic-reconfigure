98bfcb25fe03c834e0563c893e44ecf6f3a6bc6b
lxbeyond <xin.liu@tusimple.com>
2018-11-07 13:57:42 +0800

    add wait_for_service

--- include/rqt_support.hpp
+++ include/rqt_support.hpp
@@ -12,14 +12,23 @@ namespace rqt_reconfigure{
 class Client {
 public:
   Client(const rclcpp::Node::SharedPtr node, const std::string remote_name) : parameters_client(node, remote_name) {
+    try {
+    std::chrono::duration<int64_t, std::milli> timeout = std::chrono::duration<int64_t, std::milli>(500);
+    parameters_client.wait_for_service(timeout);
     auto listdata = parameters_client.list_parameters({"name"}, 0);
     for(auto name : listdata.names) {
       name_vec.push_back(name.substr(5));
     }
+    } catch (...) {
+      printf("error");
+    }
   }
   std::vector<rclcpp::Parameter> get_description(){
     std::vector<rclcpp::Parameter> description;
+    try {
     for (std::string name : name_vec) {
+      std::chrono::duration<int64_t, std::milli> timeout = std::chrono::duration<int64_t, std::milli>(500);
+      parameters_client.wait_for_service(timeout);
       auto param_des = parameters_client.get_parameters({
         "name." + name,
         "type." + name, 
@@ -33,22 +42,37 @@ public:
       });
       description.insert(description.end(), param_des.begin(), param_des.end());
     }
+    } catch(...) {
+      printf("error");
+    }
     return description;
   }
   std::vector<rclcpp::Parameter> get_values(){
     
     std::vector<rclcpp::Parameter> values;
+    try {
     for (std::string name : name_vec) {
+      std::chrono::duration<int64_t, std::milli> timeout = std::chrono::duration<int64_t, std::milli>(500);
+      parameters_client.wait_for_service(timeout);
       auto param_des = parameters_client.get_parameters({
         "value." + name,
         "type." + name,
       });
       values.insert(values.end(), param_des.begin(), param_des.end());
     }
+    } catch(...) {
+      printf("error");
+    }
     return values;
   }
   void update_params(std::vector<rclcpp::Parameter> para) {
-    auto set_parameters_results = parameters_client.set_parameters(para);
+    try {
+      std::chrono::duration<int64_t, std::milli> timeout = std::chrono::duration<int64_t, std::milli>(500);
+      parameters_client.wait_for_service(timeout);
+      auto set_parameters_results = parameters_client.set_parameters(para);
+    } catch(...) {
+      printf("error");
+    }
   }
 private:
   //static std::map<std::string, std::shared_ptr<Client>> check_map;
